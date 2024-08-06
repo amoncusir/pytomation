b4160d9709cc3df615d7a56309577d7e131da5b8
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-08-06 19:58:22 +0200

    use cli fixture

--- tests/projects/simple_root_module/test_actions_execution.py
+++ tests/projects/simple_root_module/test_actions_execution.py
@@ -1,46 +1,46 @@
 from pathlib import Path
 
-from pytomation import cli
-from pytomation.cli.arguments import Options
+# from pytomation import cli
+# from pytomation.cli.arguments import Options
+#
+#
+# def main_cli(*args: str):
+#     path = Path(__file__).parent.resolve()
+#     options = Options(_cwd=path, module_name="pytomation_module.py", verbosity=5)
+#
+#     cli.main(args, options)
 
 
-def main_cli(*args: str):
-    path = Path(__file__).parent.resolve()
-    options = Options(_cwd=path, module_name="pytomation_module.py", verbosity=5)
+def test_root_module(capsys, cli):
 
-    cli.main(args, options)
-
-
-def test_root_module(capsys):
-
-    main_cli(":root")
+    cli(":root")
 
     captured = capsys.readouterr()
 
     assert captured.err == "root", "Root module not wrote the word <root> on the standard error output"
 
 
-def test_apple_module(capsys):
+def test_apple_module(capsys, cli):
 
-    main_cli("module_apple:apple")
+    cli("module_apple:apple")
 
     captured = capsys.readouterr()
 
     assert captured.err == "apple", "Apple module not wrote the word <apple> on the standard error output"
 
 
-def test_banana_module(capsys):
+def test_banana_module(capsys, cli):
 
-    main_cli("module banana:banana")
+    cli("module banana:banana")
 
     captured = capsys.readouterr()
 
     assert captured.err == "banana", "Banana module not wrote the word <banana> on the standard error output"
 
 
-def test_spread_task_propagate(capsys):
+def test_spread_task_propagate(capsys, cli):
 
-    main_cli(":spread_action")
+    cli(":spread_action")
 
     captured = capsys.readouterr()
 
