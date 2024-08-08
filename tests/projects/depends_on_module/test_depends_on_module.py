2da8b6d3507e9209e16cd911b249b3fe4049eafc
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-08-08 22:29:03 +0200

    fix module self reference

--- tests/projects/depends_on_module/test_depends_on_module.py
+++ tests/projects/depends_on_module/test_depends_on_module.py
@@ -7,9 +7,16 @@ def test_root_modules_both_dependency(capsys, cli):
 
 
 def test_child_modules_dependency_with_specific_order(capsys, cli):
-
     cli("hydrogen:experiment")
 
     captured = capsys.readouterr()
 
     assert captured.err == "lab-hydrogen-explosion", "Child module error on dependencies"
+
+
+def test_self_module_depends_on_action(capsys, cli):
+    cli("hydrogen:print_name")
+
+    captured = capsys.readouterr()
+
+    assert captured.err == "hydrogen", "Module can't self reference"
