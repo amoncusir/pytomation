f604c46dc2fe9f854da987dec5fa5a32987ede82
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-09-09 19:15:02 +0200

    removed unused code

--- pytomation/module/action/contract.py
+++ pytomation/module/action/contract.py
@@ -40,28 +40,3 @@ class Action:
     @abstractmethod
     def get_metadata(self, mtd_type: Type) -> Any:
         raise NotImplementedError
-
-
-#
-# class FunctionAction(Action):
-#
-#     def __init__(self, fn: any):
-#         signature = inspect.signature(fn)
-#         docs = inspect.getdoc(fn)
-#         parameters = list(map(attrgetter("name"), signature.parameters.values()))
-#
-#         super().__init__(fn.__name__, parameters, docs)
-#         self.fn = fn
-#         self.fn_parameters = signature.parameters
-#
-#     def run(self, context: "Context"):
-#         self._logger.debug(f"Running action: {self.name}")
-#         self._logger.debug("\tWith context: %s", context)
-#
-#         safe_call(self.fn, context=context, **context.__dict__)
-#
-#
-# def _extract_dependencies(fn) -> Sequence[str]:
-#     if hasattr(fn, "__depends_on__"):
-#         return fn.__depends_on__
-#     return tuple()
