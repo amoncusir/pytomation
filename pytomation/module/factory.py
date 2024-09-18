56147e6fd2f693342e208c6b8a130c93985ae30d
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-09-19 00:26:41 +0200

    removed factory from wrong place

--- pytomation/module/factory.py
+++ pytomation/module/factory.py
@@ -1,16 +1,9 @@
 from abc import abstractmethod
-from typing import TYPE_CHECKING, Dict, Optional
+from typing import TYPE_CHECKING, Dict
 
 if TYPE_CHECKING:
     from pytomation.configuration import Configuration
     from pytomation.module import Module
-
-
-class ConfigurationFactory:
-
-    @abstractmethod
-    def build(self, configuration: Optional[Configuration]) -> Configuration:
-        pass
 
 
 class ModulesFactory:
