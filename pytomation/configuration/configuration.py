eeb98b89b7eab4f291404b456b1024bf87a7f2db
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-09-19 00:24:53 +0200

    update configuration.py responsibility, removed old properties and put docs on the class

--- pytomation/configuration/configuration.py
+++ pytomation/configuration/configuration.py
@@ -1,16 +1,22 @@
 import dataclasses
 from dataclasses import dataclass, field
-from pathlib import Path
 from typing import Self
 
 
 @dataclass(frozen=True)
 class Configuration:
+    """
+    Contains properties relevant to the **execution process**.
+    Like the verbosity level, the parallelization factor, the name of the modules, the module splitter (needed if
+    switches the OS), the cache patterns, etc...
+
+    Must not contain properties related to the project, like the root path, name, credentials, etc.
+
+    The current values bay becomes from different inputs and be merged with them.
+    """
 
     module_name: str = field(default="pytomation.py")
     module_path_splitter: str = field(default="/")
-    call_path: Path = field(default_factory=Path.cwd)
-    project_path: Path = field(default_factory=Path.cwd)
     verbosity: int = field(default=0)
 
     def copy(self, **changes) -> Self:
