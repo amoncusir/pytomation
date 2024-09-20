a9e41c49d986171bd3e6aaa44d1b77c531fd880e
Aran Moncusí Ramírez <aran@digitalpoet.info>
2024-09-20 03:39:03 +0200

    implemented into the initializer

--- pytomation/initializer/initializer.py
+++ pytomation/initializer/initializer.py
@@ -1,11 +1,11 @@
 from abc import abstractmethod
-from functools import partial
 from typing import Callable, List
 
+from pytomation.utils.chain import Chain, chain_process
 from pytomation.utils.store import TypedStore
 
 
-class InitializationChain:
+class InitializationChain(Chain):
 
     order: int
 
@@ -13,7 +13,7 @@ class InitializationChain:
         self.order = order
 
     @abstractmethod
-    def handle(self, next_handler: Callable[[TypedStore], None], context: TypedStore):
+    def process(self, next_handler: Callable[[TypedStore], None], context: TypedStore):
         raise NotImplementedError("Implement this method")
 
 
@@ -31,27 +31,4 @@ class InitializerHandler:
         self.chains.append(chain)
 
     def __call__(self):
-
-        last_chain = _LastChain()
-
-        iter_chains = iter(sorted(self.chains, key=lambda c: c.order))
-
-        def process_handler(context, handler: InitializationChain):
-
-            last_chain.context = context
-            next_chain = next(iter_chains, last_chain)
-            next_handler = partial(process_handler, handler=next_chain)
-
-            handler.handle(next_handler, context)
-
-        process_handler(TypedStore(), next(iter_chains, None))
-
-        return last_chain.context
-
-
-class _LastChain(InitializationChain):
-
-    context: TypedStore
-
-    def handle(self, _: None, context: TypedStore):
-        self.context = context
+        return chain_process(sorted(self.chains, key=lambda c: c.order), TypedStore())
